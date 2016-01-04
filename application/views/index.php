1699f2376d9f6f7de2f980b84dd4225291f18eeb
inside2504 <inside2504@hotmail.com>
2016-01-04 12:20:30 -0600

    página principal
    Código de página principal

---
+++ application/views/index.php
@@ -0,0 +1,64 @@
+<main>
+	<div class="head">
+		<section>
+			<h3 class="aline">Bienvenido</h3>
+		</section>
+	</div>
+	<div class="centro">
+		<section>
+			<div class="delin">
+				<div class="tit">
+					<h3>¿Qué es redes evang&eacute;licas?</h3>
+					<p>Redes evang&eacute;licas es un organismo que tiene como prop&oacute;sito establecer un espacio virtual que fomente la uni&oacute;n cristiana
+					y establezca contactos productivos que atiendan servicios e informaciones</p>
+				</div>
+			</div>
+		</section>
+	</div>
+	<div class="head">
+		<section>
+			<h3 class="aline">Servicios que ofrecemos</h3>
+		</section>
+	</div>
+	<div class="alto">
+		<section>
+			<figure class="carousel">
+				<img src="">
+			</figure>
+		</section>
+	</div>
+	<div class="head">
+		<section>
+			<h3 class="aline">Iglesias afiliadas</h3>
+		</section>
+	</div>
+	<div class="alto">
+		<section>
+			<figure>
+				<img src="">
+			</figure>
+		</section>
+	</div>
+	<div>
+		<aside class="row">
+			<aside class="col-lg-4">
+				<div class="alin">
+					<h4>Contenido</h4>
+					<a href="">Tenemos materiales para ti</a>
+				</div>
+			</aside>
+			<aside class="col-lg-4">
+				<div class="alin">
+					<h4>Cont&aacute;ctanos</h4>
+					<a href="">Da clic para ponerte en contacto con nosotros</a>
+				</div>
+			</aside>
+			<aside class="col-lg-4">
+				<div class="alin">
+					<h4>S&iacute;guenos en</h4>
+					<a href="">Facebook</a>
+				</div>
+			</aside>
+		</aside>
+	</div>
+</main>
---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<title>Letras jur&iacute;dicas</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?=base_url('')."assets/css/bootstrap.css"?>">
+	</head>
+	<body class="page">
